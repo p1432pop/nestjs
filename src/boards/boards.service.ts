cbaa3ac60b1b5510fa4a73fdad138f6143630b83
p1432pop <p1432pop@naver.com>
2024-01-10 20:45:09 +0900

    30강 : custom pipe 구현

--- src/boards/boards.service.ts
+++ src/boards/boards.service.ts
@@ -1,6 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { Board, BoardStatus } from './board.model';
 import { v1 as uuid } from 'uuid';
+import { CreateBoardDto } from './dto/create-board.dto';
 @Injectable()
 export class BoardsService {
   private boards: Board[] = [];
@@ -8,7 +9,17 @@ export class BoardsService {
   getAllBoards(): Board[] {
     return this.boards;
   }
-  createBoard(title: string, description: string) {
+
+  getBoardById(id: string): Board {
+    const found = this.boards.find((board) => board.id === id);
+    if (!found) {
+      throw new NotFoundException();
+    }
+    return found;
+  }
+
+  createBoard(createBoardDto: CreateBoardDto) {
+    const { title, description } = createBoardDto;
     const board: Board = {
       id: uuid(),
       title,
@@ -16,6 +27,17 @@ export class BoardsService {
       status: BoardStatus.PUBLIC,
     };
     this.boards.push(board);
+    return board;
+  }
+
+  deleteBoard(id: string): void {
+    const found = this.getBoardById(id);
+    this.boards = this.boards.filter((board) => board.id !== found.id);
+  }
+
+  updateBoardStatus(id: string, status: BoardStatus): Board {
+    const board = this.getBoardById(id);
+    board.status = status;
     return board;
   }
 }
---
+++ src/boards/pipes/board-status-validation.pipe.ts
@@ -0,0 +1,18 @@
+import { ArgumentMetadata, BadRequestException, PipeTransform } from '@nestjs/common';
+import { BoardStatus } from '../board.model';
+
+export class BoardStatusValidationPipe implements PipeTransform {
+  readonly StatusOption = [BoardStatus.PRIVATE, BoardStatus.PUBLIC];
+  transform(value: any) {
+    console.log(value);
+    value = value.toUpperCase();
+    if (!this.isStatusValid(value)) {
+      throw new BadRequestException();
+    }
+    return value;
+  }
+  private isStatusValid(status: any) {
+    const index = this.StatusOption.indexOf(status);
+    return index !== -1;
+  }
+}
